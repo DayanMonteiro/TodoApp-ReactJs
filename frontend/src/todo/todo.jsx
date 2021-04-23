f14d7605822db06b9bc413ed548ba81df8c6c613
Dayan Monteiro <dmsmamb@gmail.com>
2021-04-23 14:21:31 -0300

    refatorando

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -5,11 +5,20 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        /* O método bind() cria uma nova função que, quando chamada, tem sua palavra-chave this definida com o valor fornecido, com uma sequência determinada de argumentos precedendo quaisquer outros que sejam fornecidos quando a nova função é chamada */
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+    handleAdd() {
+        
+    }
+
     render() {
         return (
             <div>
                  <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                 <TodoForm />
+                 <TodoForm handleAdd={this.handleAdd} />
                  <TodoList />
             </div>
         )
